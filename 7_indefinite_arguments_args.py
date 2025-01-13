cf5ae0b2b3d975d792325a980b87f6d1d7038af6
Hernandez <ephernandez4@cps.k12.il.us>
2025-01-13 14:40:57 -0600

    changes by itzel

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,3 +1,29 @@
+# arg* = allows yout to pass multiple non-key arguments
+# **kwargs = allows you to pass multiple keyword-arguments
+#            * unpacking operator
+#            1. positional 2. default 3. keyword 4. ARBITRARY
+
+# def add(*args):
+#     total = 0
+#     for arg in args :
+#         total+=arg
+#     return total
+
+
+# print(add (1, 2, 3, 4,5,6,7))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end="")
+
+#         display_name("Spongebob", "Harold", "Squarepants", "III")
+
+def print_address(**kwargs):
+    # pass #runs program without crashing if empty
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+print_address(street="Your mums house", city="Detroit", state="MI", zipcode="12345" )
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
